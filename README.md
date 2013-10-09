95f72240a0a5fdabf4424d51c12e1749482425db
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-10-09 15:09:14 +0100

    Add initial version of xmpp-ftw-wtf

---
+++ README.md
@@ -0,0 +1,35 @@
+xmpp-ftw-wtf
+=================
+
+Unknown incoming stanza plugin for XMPP-FTW.
+
+https://github.com/lloydwatkin/xmpp-ftw.git
+
+If XMPP-FTW receives a stanza which can not be handled by any of the 
+other installed listeners then this plugin will simply push the 
+stanza (converted to a string) via the `<em>xmpp.wtf.push</em>` 
+event.
+
+# More....
+
+See the XMPP-FTW demo repository - https://github.com/lloydwatkin/xmpp-ftw-demo
+
+# Build status
+
+[![Build Status](https://secure.travis-ci.org/lloydwatkin/xmpp-ftw-wtf.png)](http://travis-ci.org/lloydwatkin/xmpp-ftw-wtf)
+
+# Install
+
+```
+npm i --save xmpp-ftw-wtf
+```
+
+# Test
+
+```
+npm test
+```
+
+# Notes
+
+Yes this plugin is mostly a joke!
---
+++ test/lib/wtf.js
@@ -0,0 +1,46 @@
+var should = require('should')
+  , Wtf = require('../../lib/wtf')
+  , ltx    = require('ltx')
+  , helper = require('../helper')
+
+describe('Wtf', function() {
+
+    var wtf, socket, xmpp, manager
+
+    before(function() {
+        socket = new helper.Eventer()
+        xmpp = new helper.Eventer()
+        manager = {
+            socket: socket,
+            client: xmpp,
+            trackId: function(id, callback) {
+                this.callback = callback
+            },
+            makeCallback: function(error, data) {
+                this.callback(error, data)
+            }
+        }
+        wtf = new Wtf()
+        wtf.init(manager)
+    })
+
+    describe('Handles', function() {
+        
+        it('Returns true for any stanza', function() {
+            wtf.handles(ltx.parse('<iq/>')).should.be.true
+        })
+        
+    })
+    
+    it('Handles incoming messages', function(done) {
+        var unknownStanza = helper.getStanza('unknown-stanza')
+        
+        socket.on('xmpp.wtf.push', function(stanza) {
+            stanza.should.equal(unknownStanza.toString())
+            done()
+        })
+        wtf.handle(unknownStanza).should.be.true
+       
+    })
+    
+})
